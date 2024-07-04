355ca1681faa6f9d3473cbdf0aa6ce90e65cf2ee
DanPaul12 <dan.paul.schechter@gmail.com>
2024-07-03 21:29:47 -0700

    hw6

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,16 +1,16 @@
 shopping_list = []
 
 while True:
-    action = input("Would you like to [A]dd an item to the list, [R]emove an item from the list, [D]isplay the full list?, or [Q]uit program: ").uppercase
+    action = input("Would you like to [A]dd an item to the list, [R]emove an item from the list, [D]isplay the full list?, or [Q]uit program: ").upper()
     if action == 'A':
         new_item = input("What item would you like to add?: ")
         shopping_list.append(new_item)
-    if action == 'R':
+    elif action == 'R':
         bad_item = input("What item would you like to remove: ")
         shopping_list.remove(bad_item)
-    if action == 'D':
+    elif action == 'D':
         print(shopping_list)
-    if action == 'Q':
+    elif action == 'Q':
         break 
     else:
         print("Invalid Input")
